353566062b6ed89feaceabb9b67d5edd2f612c09
Manish zond <manishzond07@gmail.com>
2024-03-05 14:34:19 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_project/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 // Views
 import 'package:my_project/views/Register_view.dart';
 import 'package:my_project/views/email_verify_view.dart';
@@ -36,15 +37,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // final emailVarification = user?.emailVerified ?? false;
-            // if (emailVarification) {
-            //   return const Text("Done");
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const VerifyEmailView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is Verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
 
           default:
             return const Scaffold(

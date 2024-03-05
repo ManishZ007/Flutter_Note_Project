56c25fd6b2335b5cf8013f69c37110a22b46268b
Manish zond <manishzond07@gmail.com>
2024-03-06 00:11:54 +0530

    third save commit

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:developer' as devtools show log;
+// import 'dart:developer' as devtools show log;
 
 enum MenuAction { logout }
 

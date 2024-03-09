6b9df3c0d04d93bb11a4c9fa3cd936bdf903737f
Manish zond <manishzond07@gmail.com>
2024-03-09 21:26:35 +0530

    step-9

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,13 @@
+// login exception
+class InvalidCerendialAuthException implements Exception {}
+
+// register exceptions
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
